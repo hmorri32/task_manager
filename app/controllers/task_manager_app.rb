85aa7c126d44b06b08c83c3f048731078bcbe3b2
Hugh Morrison <hmorri32@gmail.com>
2017-09-28 15:07:46 -0600

    ultra crud

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -21,9 +21,25 @@ class TaskManagerApp < Sinatra::Base
     erb :show
   end
 
+  get '/tasks/:id/edit' do 
+    @task = Task.find(params[:id])
+    erb :edit
+  end
+
   post '/tasks' do 
     task = Task.new(params[:task])
     task.save
+    redirect '/tasks'
+  end
+
+  set :method_override, true
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    Task.destroy(id.to_i)
     redirect '/tasks'
   end
 end 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,12 @@
 require 'sqlite3'
 
 class Task
-  attr_reader :title, :description
+  attr_reader :title, :description, :id
   
   def initialize(params)
     @title       = params['title']
     @description = params['description']
+    @id          = params["id"] if params["id"]
     @database    = SQLite3::Database.new('db/task_manager_development.db')
     @database_results_as_hash = true
   end
@@ -14,18 +15,34 @@ class Task
     @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)    
   end
 
-  def self.all 
+  def self.database
     database = SQLite3::Database.new('db/task_manager_development.db')
     database.results_as_hash = true
+    database
+  end
+
+  def self.all 
     tasks = database.execute("SELECT * FROM tasks")
-    
     tasks.map {|task| Task.new(task)}
   end
 
   def self.find(id)
-    database = SQLite3::Database.new('db/task_manager_development.db')
-    database.results_as_hash = true
     task = database.execute('SELECT * FROM tasks WHERE id = ?', id.to_i).first
     Task.new(task)
+  end
+
+  def self.update(id, params)
+    database.execute("UPDATE tasks
+                      SET title = ?,
+                      description = ?
+                      WHERE id = ?;",
+                      params[:title],
+                      params[:description],
+                      id)
+    Task.find(id)
+  end
+
+  def self.destroy(id)
+    database.execute('DELETE FROM tasks WHERE id = ?;', id)
   end
 end
